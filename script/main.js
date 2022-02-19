8a4acadf70fe603b1d901fcf255cacfb3f273da8
Angel Reynoso <78562338+Angelr2909@users.noreply.github.com>
2022-02-19 17:16:58 -0300

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,27 @@
+let visible = false;
+function iniciar(){
+    let elemento = document.getElementById("menu-bar");
+    
+    elemento.addEventListener("click", mostrarMenu);   
+}
+function mostrarMenu(){
+    let menu = document.getElementById("menu");
+    menu.classList.toggle("mostrar");
+}
+window.addEventListener("load", iniciar);
+
+// animado
+
+let animado = document.querySelectorAll(".animado");
+function mostrarScroll() {
+    let scrollTop = document.documentElement.scrollTop;
+    for (let i = 0; i < animado.length; i++) {
+        let alturaAnimado = animado[i].offsetTop;
+        if (alturaAnimado - 600 < scrollTop) {
+            animado[i].style.opacity = 1;
+        }
+        
+    }
+}
+window.addEventListener("scroll",mostrarScroll);
+
