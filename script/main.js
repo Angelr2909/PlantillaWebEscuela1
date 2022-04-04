602d26c5a73c8a521bc0102ce4c102e8f0ee6ed4
Angel Reynoso <78562338+Angelr2909@users.noreply.github.com>
2022-04-04 15:52:54 -0300

    Update main.js

--- script/main.js
+++ script/main.js
@@ -17,7 +17,7 @@ function mostrarScroll() {
     let scrollTop = document.documentElement.scrollTop;
     for (let i = 0; i < animado.length; i++) {
         let alturaAnimado = animado[i].offsetTop;
-        if (alturaAnimado - 400 < scrollTop) {
+        if (alturaAnimado - 800 < scrollTop) {
             animado[i].style.opacity = 1;
         }
         
